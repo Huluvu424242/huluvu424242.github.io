06fada289abda24ea4661a2af0f77addf1b9b0b9
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-03-07 18:15:39 +0100

    [feature/#2-backend]  dist rebuild
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- src/fetch-es6.worker.ts
+++ src/fetch-es6.worker.ts
@@ -62,8 +62,8 @@ export async function loadFeedData(url: string): Promise<FeedData> {
       //   'Content-Type': 'application/json'
       //   // 'Content-Type': 'application/x-www-form-urlencoded',
       // },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      // redirect: 'follow', // manual, *follow, error
+      // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       // body: JSON.stringify(data) // body data type must match "Content-Type" header
     }).then((response: Response) => {
       if (response.status != 200) {
