d43da9fd639afa0e55e10a3fba75074c6bb8f9d9
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-03-15 20:48:39 +0100

    [feature/refactoring] e2e test fixed
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- src/components/honey-news/honey-news.e2e.ts
+++ src/components/honey-news/honey-news.e2e.ts
@@ -1,88 +1,22 @@
-// import {newE2EPage} from '@stencil/core/testing';
-// import {E2EElement, E2EPage} from "@stencil/core/testing/puppeteer/puppeteer-declarations";
-// import {Logger} from "../../libs/logger";
-//
-// class TestLogger extends Logger {
-//
-// }
-//
-// describe('E2E Wrapper: voice tests of speaker', () => {
-//
-//
-//   const skippen = async () => {
-//     // Property Wert lesen
-//     const page = await newE2EPage({html: `<honey-news verbose audiolang="en" textids="3"></honey-news><p id="3">test</p>`});
-//     const stimmenAnzahl = await page.$eval('honey-news', () => {
-//       return speechSynthesis.getVoices().length;
-//     });
-//     // return true || stimmenAnzahl < 1;
-//     return stimmenAnzahl ? stimmenAnzahl < 1 : true;
-//   }
-//
-//   let skipTests: boolean;
-//   beforeAll(async () => {
-//     skipTests = await skippen();
-//   });
-//
-//
-//   // (skipTests ? describe.skip : describe)('E2E: voice tests of speaker', () => {
-//   describe('E2E: voice tests of speaker', () => {
-//
-//     let page: E2EPage;
-//     let element: E2EElement;
-//
-//     beforeEach(async () => {
-//       TestLogger.enableLogging();
-//       page = await newE2EPage({html: `<honey-news verbose audiolang="en" textids="3"></honey-news><p id="3">test</p>`});
-//       element = await page.find('honey-news');
-//     });
-//
-//     it('fire event honeySpeakerStarted', async () => {
-//       if(skipTests) return;
-//
-//       const startedEvent = await page.spyOnEvent('honeySpeakerStarted');
-//       await element.setProperty('audiolang', 'us');
-//       await page.waitForChanges();
-//       await page.click('honey-news');
-//       await page.waitForEvent('honeySpeakerStarted');
-//       expect(startedEvent).toHaveReceivedEvent();
-//     });
-//
-//     it('Has default a11y attributes', async () => {
-//       if(skipTests) return;
-//
-//       // check unpressed, rich version
-//       expect(element).toEqualAttribute('title', "Vorlesen");
-//       expect(element).toEqualAttribute('alt', "Symbol eines angehaltenen, tönenden Lautsprechers");
-//
-//       // set to press, rich version
-//       await page.click('honey-news');
-//       await page.waitForChanges();
-//
-//       // check pressed, rich version
-//       expect(element).toEqualAttribute('title', "Liest gerade vor");
-//       expect(element).toEqualAttribute('alt', "Symbol eines tönenden Lautsprechers");
-//
-//       // set to pure version
-//       element.setAttribute("pure",true);
-//       // set to unpressed
-//       await page.click('honey-news');
-//       await page.waitForChanges();
-//
-//       // check unpressed, pure version
-//       expect(element).toEqualAttribute('title', "Vorlesen");
-//       expect(element).toEqualAttribute('alt', "Symbol eines ausgeschaltenen Lautsprechers");
-//
-//       // set to pressed, pure version
-//       await page.click('honey-news');
-//       await page.waitForChanges();
-//
-//       // check pressed, pure version
-//       expect(element).toEqualAttribute('title', "Liest gerade vor");
-//       expect(element).toEqualAttribute('alt', "Symbol eines tönenden Lautsprechers");
-//
-//     });
-//
-//   });
-// });
-//
+import {newE2EPage} from '@stencil/core/testing';
+import {E2EElement, E2EPage} from "@stencil/core/testing/puppeteer/puppeteer-declarations";
+
+describe('E2E: honey-news satisfy', () => {
+
+  describe('simple check for present at page', () => {
+
+    let page: E2EPage;
+    let element: E2EElement;
+
+    beforeEach(async () => {
+      page = await newE2EPage({html: `<honey-news></honey-news>`});
+      element = await page.find('honey-news');
+    });
+
+    it('Add Feed Button is present', async () => {
+      const el = await page.find('honey-news');
+      expect(el).not.toBeNull();
+    });
+  });
+});
+
