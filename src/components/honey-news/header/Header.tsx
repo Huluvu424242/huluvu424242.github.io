d35442aec9b85bbba16ce6c9d54fdb8f4435ee2b
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-04-08 23:40:01 +0200

    [feature/#11-split]  Hinweis verschönert
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- src/components/honey-news/header/Header.tsx
+++ src/components/honey-news/header/Header.tsx
@@ -20,15 +20,21 @@ export class Header {
         <div class="row flex-spaces">
           <input class="alert-state" id="disclaimer" type="checkbox"/>
           <div class="alert alert-danger dismissible">
-              <span>!!! Das ist eine Demo Seite welche alle Feature der App zeigen soll - aus
-                diesem Grund ist
-                auch die Statistik eingeschaltet !!!
-                Es werden nur Daten zu den abgerufenen Feeds gespeichert (in memory) wie: url, anzahl der abfragen,
-                anzahl valider
-                responses
-                Sollten Sie die Speicherung nicht wünschen - dann geben Sie bitte keinen neuen Feed ein.
-                Vielen Dank für Ihr Verständnis.
-              </span>
+            <div class="row">
+              <p>!!! Das ist eine Demo Seite welche alle Feature der App zeigen soll - aus
+                diesem Grund ist auch die Statistik eingeschaltet !!!
+              </p>
+              <div class="background-warning">
+                <p>
+                  Es werden nur Daten zu den abgerufenen Feeds gespeichert (in memory) wie: url, anzahl der abfragen,
+                  anzahl valider responses
+                </p>
+                <p>
+                  Sollten Sie die Speicherung nicht wünschen - dann geben Sie bitte keinen neuen Feed ein.
+                  Vielen Dank für Ihr Verständnis.
+                </p>
+              </div>
+            </div>
             <label class="btn-close" htmlFor="disclaimer">X</label>
           </div>
         </div>
@@ -39,10 +45,11 @@ export class Header {
           <div class="collapsible">
             <input id="collapsible1" type="checkbox" name="collapsible1"/>
             <label htmlFor="collapsible1">
-              <div class="Home"></div>
+              <div class="Feeds"></div>
+              <div class="News"></div>
+              <div class="Statistik"></div>
               <div class="About"></div>
               <div class="Github"></div>
-              <div class="Credits"></div>
             </label>
             <div class="collapsible-body">
               <ul class="inline">
